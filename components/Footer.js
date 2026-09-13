ec20f5384f90dbe7080b87db94c8ef548a0c4230
0xdya <0xdya@proton.me>
2026-09-13 11:09:30 +0100

    تحديث تصميم الفوتر

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,10 @@
 import { site } from '@/lib/site';
+import Link from 'next/link';
 
 export default function Footer() {
-  const year = new Date().getFullYear();
   return (
     <footer className="site-footer">
-      <div className="site-footer-inner">
-        © {year} {site.owner}
-      </div>
+      <div>صنع من طرف: <br></br> <Link href="https://0xdya.vercel.app/" className="site-footer-inner">ضياء الدين ملوك -  {site.owner}@</Link></div>
     </footer>
   );
 }
